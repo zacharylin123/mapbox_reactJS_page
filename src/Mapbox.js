0212d371f3c3fc28590a51c38d0108f7c79e9c9f
siwei lin <zacharylin123@gmail.com>
2019-09-18 13:09:22 +1000

    added a new cluster

--- src/Mapbox.js
+++ src/Mapbox.js
@@ -68,6 +68,102 @@ export default class App extends React.Component {
             .addTo(map);
             });
 
+        // cluster1
+        map.on('load', function() {
+            // Add a new source from our GeoJSON data and set the
+            // 'cluster' option to true. GL-JS will add the point_count property to your source data.
+            map.addSource("earthquakes1", {
+            type: "geojson",
+            // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+            // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+            data: geoData,
+            cluster: true,
+            clusterMaxZoom: 14, // Max zoom to cluster points on
+            clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+            });
+             
+            map.addLayer({
+            id: "clusters",
+            type: "circle",
+            source: "earthquakes1",
+            filter: ["has", "point_count"],
+            paint: {
+            // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+            // with three steps to implement three types of circles:
+            //   * Blue, 20px circles when point count is less than 100
+            //   * Yellow, 30px circles when point count is between 100 and 750
+            //   * Pink, 40px circles when point count is greater than or equal to 750
+            "circle-color": [
+            "step",
+            ["get", "point_count"],
+            "#51bbd6",
+            100,
+            "#f1f075",
+            750,
+            "#f28cb1"
+            ],
+            "circle-radius": [
+            "step",
+            ["get", "point_count"],
+            20,
+            100,
+            30,
+            750,
+            40
+            ]
+            }
+            });
+             
+            map.addLayer({
+            id: "cluster-count",
+            type: "symbol",
+            source: "earthquakes1",
+            filter: ["has", "point_count"],
+            layout: {
+            "text-field": "{point_count_abbreviated}",
+            "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+            "text-size": 12
+            }
+            });
+             
+            map.addLayer({
+            id: "unclustered-point",
+            type: "circle",
+            source: "earthquakes1",
+            filter: ["!", ["has", "point_count"]],
+            paint: {
+            "circle-color": "#11b4da",
+            "circle-radius": 4,
+            "circle-stroke-width": 1,
+            "circle-stroke-color": "#fff"
+            }
+            });
+             
+            // inspect a cluster on click
+            map.on('click', 'clusters', function (e) {
+            var features = map.queryRenderedFeatures(e.point, { layers: ['clusters'] });
+            var clusterId = features[0].properties.cluster_id;
+            map.getSource('earthquakes1').getClusterExpansionZoom(clusterId, function (err, zoom) {
+            if (err)
+            return;
+             
+            map.easeTo({
+            center: features[0].geometry.coordinates,
+            zoom: zoom
+            });
+            });
+            });
+             
+            map.on('mouseenter', 'clusters', function () {
+            map.getCanvas().style.cursor = 'pointer';
+            });
+            map.on('mouseleave', 'clusters', function () {
+            map.getCanvas().style.cursor = '';
+            });
+            });
+
+
+        // clusert2
         map.on('load', function () {
         // add a clustered GeoJSON source for a sample set of earthquakes
         map.addSource('earthquakes', {
